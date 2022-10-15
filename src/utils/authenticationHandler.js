de8c4b551a3d4b409a6cc0a3e1fba1c236673eae
Pete Schultz <pschultz010@gmail.com>
2022-10-14 17:05:54 -0700

    first commit

---
+++ src/utils/authenticationHandler.js
@@ -0,0 +1,46 @@
+import {authorize, refresh} from 'react-native-app-auth';
+import Config from "react-native-config";
+
+class AuthenticationHandler {
+  constructor() {
+    this.spotifyAuthConfig = {
+      clientId: Config.CLIENT_ID,
+      clientSecret: Config.CLIENT_SECRET,
+      redirectUrl: Config.REDIRECT_URI,
+      scopes: [
+        'playlist-read-private',
+        'playlist-modify-public',
+        'playlist-modify-private',
+        'user-library-read',
+        'user-library-modify',
+        'user-top-read',
+      ],
+      serviceConfiguration: {
+        authorizationEndpoint: Config.AUTH_ENDPOINT,
+        tokenEndpoint: Config.TOKEN_ENDPOINT,
+      },
+    };
+  }
+
+  async onLogin() {
+    try {
+      const result = await authorize(this.spotifyAuthConfig);
+      alert(JSON.stringify(result));
+      return result;
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    } 
+  }
+
+  async refreshLogin(refreshToken) {
+    const result = await refresh(this.spotifyAuthConfig, {
+      refreshToken: refreshToken,
+    });
+    return result;
+  }
+
+}
+
+const authHandler = new AuthenticationHandler();
+
+export default authHandler;
